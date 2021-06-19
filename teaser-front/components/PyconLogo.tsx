6d36df2b69966155a1d2d69fc09c16ea218fac19
Climier-code <ikowhddus@naver.com>
2021-06-19 14:38:44 +0900

    Add SemiPyconlogo-long without python-logo

---
+++ teaser-front/components/PyconLogo.tsx
@@ -0,0 +1,57 @@
+/** @jsxImportSource @emotion/react */
+import styled from '@emotion/styled';
+import { css } from "@emotion/react";
+
+const PyconLogoBlock = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+const TextBlock = styled.div`
+    font-weight: 700;
+    border-radius: 6rem;
+    border: 1px solid #f5df4d;
+    font-size: 5rem;
+    padding: 2rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    text-align: center;
+    vertical-align: middle;
+    margin: 1rem;
+`;
+
+
+function PyconLogo() {
+    return (
+        <PyconLogoBlock>
+            <TextBlock 
+                css={css`
+                    border-color: #939597;
+                    color: #939597;
+                `}
+                >ic</TextBlock>
+            <TextBlock
+                css={css`
+                    padding-bottom: 2rem;
+                `}
+                >py</TextBlock>
+            <TextBlock
+                css={css`
+                    padding-bottom: 2rem;
+                `}
+                >con</TextBlock>
+            <TextBlock 
+                css={css`
+                    border-color: #939597;
+                    color: #939597;
+                `}
+                >kr</TextBlock>
+            <TextBlock>2021</TextBlock>
+        </PyconLogoBlock>
+        
+    )
+}
+
+export default PyconLogo;
