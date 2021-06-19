d8759029716f30bb363ae78431d8fb81de42eea3
Climier-code <ikowhddus@naver.com>
2021-06-19 17:02:17 +0900

    Add AboutPage without Footer

---
+++ teaser-front/pages/about/index.tsx
@@ -0,0 +1,13 @@
+import Header from '../../components/Header';
+import PyconLogo from '../../components/PyconLogo';
+import AboutContext from '../../components/about/AboutContext';
+
+export default function about() {
+    return (
+        <div>
+            <Header />
+            <PyconLogo />
+            <AboutContext />
+        </div>
+    )
+}
