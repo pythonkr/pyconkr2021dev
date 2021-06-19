67c24c80f42974faec73b752ae3f6ba23b1de01d
Climier-code <ikowhddus@naver.com>
2021-06-19 13:48:54 +0900

    Add Header

---
+++ teaser-front/components/Header.tsx
@@ -0,0 +1,97 @@
+import Link from 'next/link';
+import styled from '@emotion/styled';
+
+
+type CategoriesType = CategoryType[]
+
+type CategoryType = {
+    name: string;
+    text: string;
+}
+
+const categories:CategoriesType = [
+    {
+        name: 'about',
+        text: 'ABOUT'
+    },
+    {
+        name: 'contribute',
+        text: '기여하기'
+    },
+    {
+        name: 'support',
+        text: '후원하기'
+    },
+    {
+        name: 'coc',
+        text: '행동강령'
+    }
+]
+
+
+
+const Positionier = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    top: 0px;
+    width: 100%;
+`;
+
+const BlackBackground = styled.div`
+    background: black;
+    display: flex;
+    justify-content: center;
+    height: auto;
+    align-items: center;
+`;
+
+const HeaderContents = styled.div`
+    width: 1280px;
+    height: 88px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    padding-right: 1rem;
+    padding-left: 1rem;
+`;
+
+const Category = styled.div`
+    font-size: 1.25rem;
+    letter-spacing: 2px;
+    color: #F5DF4D;
+    font-weight: 700;
+    margin: 3rem;
+    text-align: center;
+`;
+
+const Spacer = styled.div`
+    flex-grow: 1;
+`;
+
+function Header () {
+    return(
+        <Positionier>
+            <BlackBackground>
+                <HeaderContents>
+                    <Category>
+                        <Link href="/">
+                            <a>PYCON KR 2021</a>
+                        </Link>
+                    </Category>
+                    <Spacer />
+                    {categories.map(category => (
+                        <Category key={category.name}>
+                            <Link href={`/${category.name}`}>
+                                <a>{category.text}</a>
+                                </Link>
+                        </Category>
+                    ))}
+                </HeaderContents>
+            </BlackBackground>
+        </Positionier>
+    );
+}
+
+export default Header;
--- teaser-front/pages/index.tsx
+++ teaser-front/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <Header />
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
