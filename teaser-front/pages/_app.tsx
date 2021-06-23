9726e46d8ce157ff8bcb40ddb99eaf5706508bd9
Climier-code <ikowhddus@naver.com>
2021-06-23 21:13:54 +0900

    Add First Mobile View zoom=0

--- teaser-front/pages/_app.tsx
+++ teaser-front/pages/_app.tsx
@@ -8,7 +8,8 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>파이콘 한국 2021</title>
         <link rel="icon" href="/img/profile_wht.png" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       </Head>
       <Component {...pageProps} />
     </>
