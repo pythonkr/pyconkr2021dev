7ad936cc41b23fb507ac629d31f965a1cf342c5d
Climier-code <ikowhddus@naver.com>
2021-06-23 22:10:36 +0900

    Fix main:Gotosupport

--- teaser-front/components/main/GotoSection.tsx
+++ teaser-front/components/main/GotoSection.tsx
@@ -24,7 +24,7 @@ export function GotoSupport () {
         <ContentBackgroundBlock>
             <GotoSectionBlock>
                 <h1>후원하기</h1>
-                <p><Link href={`/contribute`}>파이콘 한국 2021 후원 안내 →</Link></p>
+                <p><Link href={`/support`}>파이콘 한국 2021 후원 안내 →</Link></p>
             </GotoSectionBlock>
         </ContentBackgroundBlock>
     )
