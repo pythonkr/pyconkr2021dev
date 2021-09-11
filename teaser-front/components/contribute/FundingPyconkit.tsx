0948ad56245dc8d87c279427c10e841453f9120f
Parkseongheum <ab6449@gmail.com>
2021-09-12 00:19:20 +0900

    굿즈 comming-soon 삭제 및 참가버튼 추가

--- teaser-front/components/contribute/FundingPyconkit.tsx
+++ teaser-front/components/contribute/FundingPyconkit.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 import {GuideContributeBlock} from './GotoContribute';
 import { ContentBackgroundBlock } from '../ContentBackground';
+import { RegisterButtonBlock } from '../main/MainView';
 const AboutFundingBlock = styled.div`
     height: 25rem;
     background-color: #f5df4d;
@@ -13,16 +14,29 @@ const ComingSoonBlock = styled.h3`
 
 function FundingPyconkit () {
     return(
-        <ContentBackgroundBlock>
-            <GuideContributeBlock>
-                <div>
-                    <h1>PYCON KOREA 2021 KIT</h1>
-                    <h3>파이콘 한국 2021 굿즈 펀딩하기</h3>
-                    <ComingSoonBlock>Coming Soon...</ComingSoonBlock>
-                </div>
-            </GuideContributeBlock>
-        </ContentBackgroundBlock>
-        
+        <>
+            <ContentBackgroundBlock>
+                <GuideContributeBlock>
+                    <div>
+                        <h1>PYCON KOREA 2021 KIT</h1>
+                        <h3>
+                            파이콘 한국 2021 굿즈 펀딩하기
+                        </h3>
+                        {/* <ComingSoonBlock>Coming Soon...</ComingSoonBlock> */}
+                        <p>파이콘 한국 2021 굿즈는 참가등록시 함께 구입하실 수 있습니다.</p>
+                    </div>
+                    <RegisterButtonBlock style={{width: 200, padding: 2, fontSize: 20}}>
+                                <a href="https://festa.io/events/1788/">참가등록</a>
+                    </RegisterButtonBlock>
+                </GuideContributeBlock>
+            </ContentBackgroundBlock>
+
+            {/* <ContentBackgroundBlock>
+                <RegisterButtonBlock>
+                    <a href="https://festa.io/events/1788/">참가 등록</a>
+                </RegisterButtonBlock>
+            </ContentBackgroundBlock> */}
+        </>
     )
 }
 
