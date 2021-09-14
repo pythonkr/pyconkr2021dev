827689d1af8cebdb482363e3d00fc8422cbd3e0b
Parkseongheum <ab6449@gmail.com>
2021-09-14 23:46:26 +0900

    공지사항 기능 적용

--- teaser-front/pages/index.tsx
+++ teaser-front/pages/index.tsx
@@ -4,16 +4,37 @@ import FundingPyconkit from '../components/contribute/FundingPyconkit'
 import GotoContribute from '../components/contribute/GotoContribute'
 import Header from '../components/Header'
 import MainView from '../components/main/MainView'
-import AboutContext from '../components/about/AboutContext'
+import AboutContext, { AboutBlock } from '../components/about/AboutContext'
 import { GotoSupport, GotoCoc } from '../components/main/GotoSection'
 import PyconLogo from '../components/PyconLogo'
 import { ContentBackgroundBlock } from '../components/ContentBackground'
+import Sponsor from '../components/api/Sponsor'
 
-export default function Home() {
+export default function Home({ data, data_sponsor } : any) {
   return (
     <>
         <Header />
         <MainView />
+        <ContentBackgroundBlock>
+            <div>
+            <AboutBlock>
+              
+                <h1>공지사항</h1> 
+
+                    {
+                        data.map((d : any) => (
+                            <p key={d.title}>
+                                <a href={d.link}>{d.title}</a>
+                            </p>
+                        ))
+                    }
+            </AboutBlock>
+            </div>
+
+        </ContentBackgroundBlock>
+        
+        <Sponsor data_sponsor={data_sponsor} />
+
         <GotoContribute />
         <FundingPyconkit />
         <AboutContext />
@@ -29,4 +50,34 @@ export default function Home() {
       
     </>
   )
+}
+
+
+export async function getStaticProps() {
+  let url = process.env.ENV == 'DEV' ? 'https://dev.2021.api.pycon.kr/api/v1' : 'https://2021.api.pycon.kr/api/v1'
+
+  if (process.env.ENV == 'LOCAL') {
+    url = 'http://127.0.0.1:8000/api/v1'
+  }
+
+  const res = await fetch(url + '/article')
+  const data = await res.json()
+
+  const res_sponsor = await fetch(url + '/sponsors')
+  const data_sponsor = await res_sponsor.json()
+
+  if (!data || !data_sponsor) {
+    return {
+      notFound: true,
+    }
+  }
+
+  console.log('notice_data: ', data)
+  console.log('sponsor_data: ', data_sponsor)
+
+  return {
+    props: { data: data, 
+             data_sponsor: data_sponsor 
+    }, // will be passed to the page component as props
+  }
 }
