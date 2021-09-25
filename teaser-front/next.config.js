97b3d428df5fd22434b7a397cd5953d48f251792
Parkseongheum <ab6449@gmail.com>
2021-09-25 18:01:09 +0900

    Revert "Image Src 도메인 적용"
    This reverts commit 22be4405e98b51eb8581d0551f31ad5394e4f229.

--- teaser-front/next.config.js
+++ teaser-front/next.config.js
@@ -4,8 +4,7 @@ module.exports = {
     trailingSlash: true,
     images: {
         disableStaticImages: true,
-        // loader: "imgix",
-        // path: "",
-        domains: ['2021.pycon.kr', '2021devdev.pycon.kr', 'localhost']
+        loader: "imgix",
+        path: "",
     },
 };
