733580864fb2dfc2a3929ccf7eef9eb121a9a918
constmoon <jiwoo.link@gmail.com>
2021-09-26 21:13:52 +0900

    open graph 추가

--- teaser-front/pages/_app.tsx
+++ teaser-front/pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import Head from 'next/head';
-import styled from '@emotion/styled';
+import Head from 'next/head'
+import styled from '@emotion/styled'
+import { NextSeo } from 'next-seo'
 
 const GlobalLayout = styled.div`
   max-width: 1280px;
@@ -14,12 +15,28 @@ const GlobalLayout = styled.div`
 function App({ Component, pageProps }: AppProps) {
   return (
     <GlobalLayout>
-      <Head>
-        <title>파이콘 한국 2021</title>
-        <link rel="icon" href="/img/profile_wht.png" />
-        <meta name="viewport" content="width=device-width" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      </Head>
+      <NextSeo
+        title="파이콘 한국 2021"
+        description="파이콘 한국은 한국의 파이썬 개발자들이 지식을 공유하고 만남을 갖기 위한 장입니다. 파이콘 한국 2021은 온라인으로 진행됩니다.'"
+        openGraph={{
+          type: 'website',
+          url: 'https://2021.pycon.kr',
+          site_name: '파이콘 한국 2021',
+          description: '파이콘 한국은 한국의 파이썬 개발자들이 지식을 공유하고 만남을 갖기 위한 장입니다. 파이콘 한국 2021은 온라인으로 진행됩니다.',
+          images: [{
+            url: 'https://2021.pycon.kr/img/thumbnail.jpg'
+          }]
+        }}
+        twitter={{
+          cardType: 'summary_large_image'
+        }}
+        additionalLinkTags={[
+          {
+            rel: 'icon',
+            href: 'https://2021.pycon.kr/img/profile_wht.png'
+          }
+        ]}
+      />
       <Component {...pageProps} />
     </GlobalLayout>
   )
