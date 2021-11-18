138c5d24df074e3299bb1fd9d1c08c3ba47e3a7f
Climier-code <ikowhddus@naver.com>
2021-11-19 00:50:33 +0900

    fix: right propsFormat in writeFileSync Func

--- teaser-front/script/sitemap.js
+++ teaser-front/script/sitemap.js
@@ -30,7 +30,7 @@ const formatted = (sitemap) => prettier.format(sitemap, { parser: 'html' });
     </sitemapindex>
   `;
 
-  const formattedSitemap = [formatted(sitemap)];
+  const formattedSitemap = formatted(sitemap);
 
   fs.writeFileSync('../public/sitemap.xml', formattedSitemap, 'utf8');
 })();
