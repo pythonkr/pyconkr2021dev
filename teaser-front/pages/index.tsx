536227556113d4be03dddca997bc1cb98fa67afa
Climier-code <ikowhddus@naver.com>
2021-11-21 23:22:37 +0900

    fix: img path about dot, profile

--- teaser-front/pages/index.tsx
+++ teaser-front/pages/index.tsx
@@ -1,54 +1,66 @@
-import Image from 'next/image'
-import Header from '../components/Header'
-import MainView from '../components/main/MainView'
-import AboutContext from '../components/about/AboutContext'
-import PyconLogo from '../components/PyconLogo'
-import { ContentBackgroundBlock } from '../components/ContentBackground'
+import Image from 'next/image';
+import Header from '../components/Header';
+import MainView from '../components/main/MainView';
+import AboutContext from '../components/about/AboutContext';
+import PyconLogo from '../components/PyconLogo';
+import { ContentBackgroundBlock } from '../components/ContentBackground';
 import MainNotice from '../components/main/MainNotice';
 
-export default function Home({ notice_data, data_sponsor } : any) {
+export default function Home({ notice_data, data_sponsor }: any) {
   return (
     <>
-        <Header />
-        <MainView />
-        <ContentBackgroundBlock>
-            <MainNotice noticeData={notice_data} />
-        </ContentBackgroundBlock>
-        <AboutContext />
-        <ContentBackgroundBlock>
-          <Image src="/img/dot1.png" width={700} height={100} alt="dot1" layout="responsive" />
-        </ContentBackgroundBlock>
-          <PyconLogo />
-        <ContentBackgroundBlock>
-          <Image src="/img/dot2.png" width={700} height={100} alt="dot1" layout="responsive" />
-        </ContentBackgroundBlock>
+      <Header />
+      <MainView />
+      <ContentBackgroundBlock>
+        <MainNotice noticeData={notice_data} />
+      </ContentBackgroundBlock>
+      <AboutContext />
+      <ContentBackgroundBlock>
+        <Image
+          src="https://2021.pycon.kr/img/dot1.png"
+          width={700}
+          height={100}
+          alt="dot1"
+          layout="responsive"
+        />
+      </ContentBackgroundBlock>
+      <PyconLogo />
+      <ContentBackgroundBlock>
+        <Image
+          src="https://2021.pycon.kr/img/dot2.png"
+          width={700}
+          height={100}
+          alt="dot2"
+          layout="responsive"
+        />
+      </ContentBackgroundBlock>
     </>
-  )
+  );
 }
 
-
 export async function getStaticProps() {
-  let url = process.env.ENV == 'DEV' ? 'https://dev.2021.api.pycon.kr/api/v1' : 'https://2021.api.pycon.kr/api/v1'
+  let url =
+    process.env.ENV == 'DEV'
+      ? 'https://dev.2021.api.pycon.kr/api/v1'
+      : 'https://2021.api.pycon.kr/api/v1';
 
   if (process.env.ENV == 'LOCAL') {
-    url = 'http://127.0.0.1:8000/api/v1'
+    url = 'http://127.0.0.1:8000/api/v1';
   }
 
-  const res = await fetch(url + '/article')
-  const data = await res.json()
+  const res = await fetch(url + '/article');
+  const data = await res.json();
 
-  const res_sponsor = await fetch(url + '/sponsors')
-  const data_sponsor = await res_sponsor.json()
+  const res_sponsor = await fetch(url + '/sponsors');
+  const data_sponsor = await res_sponsor.json();
 
   if (!data || !data_sponsor) {
     return {
       notFound: true,
-    }
+    };
   }
 
   return {
-    props: { notice_data: data,
-             data_sponsor: data_sponsor
-    }, // will be passed to the page component as props
-  }
+    props: { notice_data: data, data_sponsor: data_sponsor }, // will be passed to the page component as props
+  };
 }
